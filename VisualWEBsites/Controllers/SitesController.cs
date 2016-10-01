469ecbfe95aa6edd51329b8d10096ce8f40fdb00
Merikjo <johanna.merikanto@stud.pointcollege.fi>
2016-10-01 12:11:40 +0300

    Asennetut päivitykset uuteen projektiin 1.10.2016.

---
+++ VisualWEBsites/Controllers/SitesController.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisualWEBsites.Controllers
+{
+    public class SitesController : Controller
+    {
+        // GET: Sites
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Talvi
+        public ActionResult Talvi()
+        {
+            return View();
+        }
+
+        // GET: Sivu2
+        public ActionResult Kevät()
+        {
+            return View();
+        }
+
+        // GET: Sivu3
+        public ActionResult Kesä()
+        {
+            return View();
+        }
+        public ActionResult Syksy()
+        {
+            return View();
+        }
+
+        public ActionResult Ajankulua()
+        {
+            return View();
+        }
+
+        public ActionResult Timegoesby()
+        {
+            return View();
+        }
+
+        public ActionResult TalviImage1()
+        {
+            return View();
+        }
+
+        public ActionResult TalviImage2()
+        {
+            return View();
+        }
+
+        public ActionResult TalviImage3()
+        {
+            return View();
+        }
+
+        public ActionResult Talvikuvat()
+        {
+            return View();
+        }
+
+        public ActionResult Kevatkuvat()
+        {
+            return View();
+        }
+
+        public ActionResult Kesakuvat()
+        {
+            return View();
+        }
+
+        public ActionResult Syksykuvat()
+        {
+            return View();
+        }
+        public ActionResult KevatImage1()
+        {
+            return View();
+        }
+
+        public ActionResult YearsGoOn()
+        {
+            return View();
+        }
+
+        public ActionResult KesaImage1()
+        {
+            return View();
+        }
+
+        public ActionResult Vuodenaika()
+        {
+            return View();
+        }
+        public ActionResult Vuodenaika2()
+        {
+            return View();
+        }
+        public ActionResult Vuodenaika3()
+        {
+            return View();
+        }
+        public ActionResult RautaAika()
+        {
+            return View();
+        }
+        public ActionResult AikaTapit()
+        {
+            return View();
+        }
+        public ActionResult Ketju()
+        {
+            return View();
+        }
+        public ActionResult KesäKaisla()
+        {
+            return View();
+        }
+        public ActionResult KesäVuokot()
+        {
+            return View();
+        }
+        public ActionResult KesäVuokot2()
+        {
+            return View();
+        }
+        public ActionResult Voimala()
+        {
+            return View();
+        }
+        public ActionResult KevätHeijaste()
+        {
+            return View();
+        }
+        public ActionResult KevätHeijaste2()
+        {
+            return View();
+        }
+        public ActionResult KevätHeijaste3()
+        {
+            return View();
+        }
+        public ActionResult KevätHeijaste4()
+        {
+            return View();
+        }
+        public ActionResult KesäHaituva()
+        {
+            return View();
+        }
+        public ActionResult KesäHaituva2()
+        {
+            return View();
+        }
+        public ActionResult KesäHaituva3()
+        {
+            return View();
+        }
+        public ActionResult Ikkuna()
+        {
+            return View();
+        }
+        public ActionResult TyöPaja()
+        {
+            return View();
+        }
+        public ActionResult OppositeSite()
+        {
+            return View();
+        }
+        public ActionResult VesiKivi()
+        {
+            return View();
+        }
+        public ActionResult Syyskaisla()
+        {
+            return View();
+        }
+        public ActionResult Ketjussa()
+        {
+            return View();
+        }
+        public ActionResult Ketjutus()
+        {
+            return View();
+        }
+        public ActionResult ÄlyPuhelin()
+        {
+            return View();
+        }
+        public ActionResult KevätKaiPuu()
+        {
+            return View();
+        }
+    }
+}
+    }
+}
