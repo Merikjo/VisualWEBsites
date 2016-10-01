122b763ab5784aa3b8976bf988f18c3e943cbf78
Merikjo <johanna.merikanto@stud.pointcollege.fi>
2016-10-01 12:04:34 +0300

    Add project files.

---
+++ VisualWEBsites/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisualWEBsites.Startup))]
+namespace VisualWEBsites
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
